5c39bed79454e485d30c9172662be7b72201a15f
Dark Dong <viva@163.com>
2018-04-09 14:05:45 +0800

    config truffle on rinkeby to address the issue: Error: The contract code couldn't be stored, please check your gas amount.

--- blockchain/truffle.js
+++ blockchain/truffle.js
@@ -5,7 +5,15 @@ module.exports = {
         development: {
             host: "127.0.0.1",
             port: 7545,
-            network_id: "*" // Match any network id
+            network_id: "*"
+        },
+        rinkeby: {
+            host: "127.0.0.1", // Connect to geth on the specified
+            port: 8545,
+            from: "0x853dd08d2c3c871ea896e09b3f36eed73c622946", // default address to use for any transaction Truffle makes during migrations
+            network_id: 4,
+            gas: 6712388,
+            gasPrice: 65000000000,
         }
     }
 };
